ab5d4964c6e89748089018150240453e0aa024e1
Alexander Costa Filho <alexandercostafilho@gmail.com>
2022-11-15 23:16:15 -0300

    Início frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TaskListComponent} from "./tasks/task-list/task-list.component";
+import {TaskListFormComponent} from "./tasks/task-form/task-list-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: TaskListComponent},
+  {path:'cadastrar', component: TaskListFormComponent},
+  {path:'edit/:id', component: TaskListFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
